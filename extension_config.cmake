1b50611fa4be010fea5a522c18c364539db47337
Felix <23635466+its-felix@users.noreply.github.com>
2025-12-16 14:14:27 +0100

    update to duckdb v1.4.3

--- extension_config.cmake
+++ extension_config.cmake
@@ -2,15 +2,14 @@ duckdb_extension_load(core_functions)
 duckdb_extension_load(parquet)
 duckdb_extension_load(json)
 duckdb_extension_load(icu)
-# https://github.com/duckdb/duckdb/blob/v1.4.2/.github/config/extensions/httpfs.cmake
+# https://github.com/duckdb/duckdb/blob/v1.4.3/.github/config/extensions/httpfs.cmake
 duckdb_extension_load(httpfs
     LOAD TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 041a782b0b33495448a7eaa68973cf8c2174feb6
+    GIT_TAG 9c7d34977b10346d0b4cbbde5df807d1dab0b2bf
     INCLUDE_DIR src/include
-    APPLY_PATCHES
 )
-# https://github.com/duckdb/duckdb/blob/v1.4.2/.github/config/extensions/aws.cmake
+# https://github.com/duckdb/duckdb/blob/v1.4.3/.github/config/extensions/aws.cmake
 duckdb_extension_load(aws
     GIT_URL https://github.com/duckdb/duckdb-aws
     GIT_TAG 55bf3621fb7db254b473c94ce6360643ca38fac0
